2b241148ed20d249b4e7f4587f5a3856f8df0960
royb_tabit <roy@tabit.cloud>
2021-04-12 14:01:06 +0300

    add back event proc

--- index.js
+++ index.js
@@ -92,6 +92,63 @@ var handleCloudWatch = function(event, context) {
   return _.merge(slackMessage, baseSlackMessage);
 };
 
+var processEvent = function(event, context) {
+  console.log("sns received:" + JSON.stringify(event, null, 2));
+  var slackMessage = null;
+  var eventSubscriptionArn = event.Records[0].EventSubscriptionArn;
+  var eventSnsSubject = event.Records[0].Sns.Subject || 'no subject';
+  var eventSnsMessageRaw = event.Records[0].Sns.Message;
+  var eventSnsMessage = null;
+
+  try {
+    eventSnsMessage = JSON.parse(eventSnsMessageRaw);
+  }
+  catch (e) {
+  }
+
+  if(eventSubscriptionArn.indexOf(config.services.codepipeline.match_text) > -1 || eventSnsSubject.indexOf(config.services.codepipeline.match_text) > -1 || eventSnsMessageRaw.indexOf(config.services.codepipeline.match_text) > -1){
+    console.log("processing codepipeline notification");
+    slackMessage = handleCodePipeline(event,context)
+  }
+  else if(eventSubscriptionArn.indexOf(config.services.elasticbeanstalk.match_text) > -1 || eventSnsSubject.indexOf(config.services.elasticbeanstalk.match_text) > -1 || eventSnsMessageRaw.indexOf(config.services.elasticbeanstalk.match_text) > -1){
+    console.log("processing elasticbeanstalk notification");
+    slackMessage = handleElasticBeanstalk(event,context)
+  }
+  else if(eventSnsMessage && 'AlarmName' in eventSnsMessage && 'AlarmDescription' in eventSnsMessage){
+    console.log("processing cloudwatch notification");
+    slackMessage = handleCloudWatch(event,context);
+  }
+  else if(eventSubscriptionArn.indexOf(config.services.codedeploy.match_text) > -1 || eventSnsSubject.indexOf(config.services.codedeploy.match_text) > -1 || eventSnsMessageRaw.indexOf(config.services.codedeploy.match_text) > -1){
+    console.log("processing codedeploy notification");
+    slackMessage = handleCodeDeploy(event,context);
+  }
+  else if(eventSubscriptionArn.indexOf(config.services.elasticache.match_text) > -1 || eventSnsSubject.indexOf(config.services.elasticache.match_text) > -1 || eventSnsMessageRaw.indexOf(config.services.elasticache.match_text) > -1){
+    console.log("processing elasticache notification");
+    slackMessage = handleElasticache(event,context);
+  }
+  else if(eventSubscriptionArn.indexOf(config.services.autoscaling.match_text) > -1 || eventSnsSubject.indexOf(config.services.autoscaling.match_text) > -1 || eventSnsMessageRaw.indexOf(config.services.autoscaling.match_text) > -1){
+    console.log("processing autoscaling notification");
+    slackMessage = handleAutoScaling(event, context);
+  }
+  else{
+    slackMessage = handleCatchAll(event, context);
+  }
+
+  postMessage(slackMessage, function(response) {
+    if (response.statusCode < 400) {
+      console.info('message posted successfully');
+      context.succeed();
+    } else if (response.statusCode < 500) {
+      console.error("error posting message to slack API: " + response.statusCode + " - " + response.statusMessage);
+      // Don't retry because the error is due to a problem with the request
+      context.succeed();
+    } else {
+      // Let Lambda retry
+      context.fail("server error when processing message: " + response.statusCode + " - " + response.statusMessage);
+    }
+  });
+};
+
 exports.handler = function(event, context) {
     processEvent(event, context);
 };
